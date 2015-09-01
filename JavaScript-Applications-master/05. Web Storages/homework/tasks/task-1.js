8a5fb6036238a348ca8c66bef4677df11a11631c
Iliya Nedelchev <Il.Nedelchev@gmail.com>
2015-09-02 00:23:29 +0300

    complete

--- JavaScript-Applications-master/05. Web Storages/homework/tasks/task-1.js
+++ JavaScript-Applications-master/05. Web Storages/homework/tasks/task-1.js
@@ -4,61 +4,91 @@ function solve() {
     var _generateNumberArray;
     var _countOfTry;
 
-  function init(playerName, endCallback) {
-      _playerName = playerName;
-      _endCallback = endCallback;
-      _generateNumberArray = [];
-      _countOfTry = 0;
+    function init(playerName, endCallback) {
+        _playerName = playerName;
+        _endCallback = endCallback;
+        _generateNumberArray = [];
+        _countOfTry = 0;
 
-      for (var i = 0; i < 4; i++) {
-          _generateNumberArray.push(Math.floor((Math.random() * 10) + 1));
-      }
-  }
+        for (var i = 0; i < 4; i++) {
+            _generateNumberArray.push(Math.floor((Math.random() * 10) + 1));
+        }
+    }
 
-  function guess(number) {
-      if (_playerName == undefined) {
-          throw "not init";
-      }
+    function guess(number) {
+        if (_playerName == undefined) {
+            throw "not init";
+        }
 
-      _countOfTry++;
+        _countOfTry++;
 
-      var sheeps = 0;
-      var rams = 0;
-      var i = 0;
-      var j = 0;
+        var sheeps = 0;
+        var rams = 0;
+        var i = 0;
+        var j = 0;
 
-      var stringNumber = number.toString();
+        var stringNumber = number.toString();
 
-      for (i = 0;  i < 4; i ++) {
-          var digit = +stringNumber.charAt(i);
+        for (i = 0; i < 4; i++) {
+            var digit = +stringNumber.charAt(i);
 
-          if (_generateNumberArray[i] == digit) {
-              rams++;
-              continue;
-          }
+            if (_generateNumberArray[i] == digit) {
+                rams++;
+                continue;
+            }
 
-          for (j = 0; j < 4; j ++) {
-              if (_generateNumberArray[j] == digit) {
-                  sheeps++;
-                  break;
-              }
-          }
-      }
+            for (j = 0; j < 4; j++) {
+                if (_generateNumberArray[j] == digit) {
+                    sheeps++;
+                    break;
+                }
+            }
+        }
 
-      if (rams == 4) {
-          _endCallback();
-      }
+        if (rams == 4) {
+            var highScore = JSON.parse(localStorage.getItem("highScore")) || new HighScore();
 
-      return { sheep: sheeps, rams: rams }
-  }
+            highScore.addPlayer(_playerName, _countOfTry);
 
-  function getHighScore(count) {
+            localStorage.setItem("highScore", JSON.stringify(highScore));
 
-  }
+            _endCallback();
+        }
 
-  return {
-    init, guess, getHighScore
-  }
+        return { sheep: sheeps, rams: rams }
+    }
+
+    function getHighScore(count) {
+        if (count <= 0) {
+            throw "count error";
+        }
+
+        var highScore = JSON.parse(localStorage.getItem("highScore")) || new HighScore();
+
+        $(highScore.players).slice(count);
+    }
+
+    var HighScore = function () {
+        this.players = [];
+    };
+
+    HighScore.prototype.addPlayer = function (name, score) {
+        var player = new Player(name, score);
+
+        this.players.push(player);
+
+        _.sortBy(this.players, "score");
+    }
+ 
+
+    var Player = function (name, score) {
+        this.name = name;
+        this.score = score;
+    };
+
+    return {
+        init, guess, getHighScore
+    }
 }
 
 
