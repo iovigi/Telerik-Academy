9d79666597e2b8a1d38d4d6b81374eb41b8d83cb
Iliya Nedelchev <Il.Nedelchev@gmail.com>
2015-11-06 00:23:12 +0200

    workshop

---
+++ Data-Structures-and-Algorithms-master/workshops/2015-10-30-data-structures/jedi-meditation/Workshop/Workshop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] jedis = Console.ReadLine().Split(' ');
+
+            var jediMasters = jedis.Where(x => x.Contains("m"));
+            var jediKnights = jedis.Where(x => x.Contains("k"));
+            var padawans = jedis.Where(x => x.Contains("p"));
+
+            Console.WriteLine("{0} {1} {2}",string.Join(" ",jediMasters),string.Join(" ",jediKnights), string.Join(" ", padawans));
+        }
+    }
+}
