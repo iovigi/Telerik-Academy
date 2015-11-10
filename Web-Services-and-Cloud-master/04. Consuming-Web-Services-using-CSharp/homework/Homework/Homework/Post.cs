148f9a2aa55b43f808658ed0517da42f8435beaf
iovigi <il.nedelchev@gmail.com>
2015-11-10 19:31:13 +0200

    homework

---
+++ Web-Services-and-Cloud-master/04. Consuming-Web-Services-using-CSharp/homework/Homework/Homework/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework
+{
+    public class Post
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("post was written by user with id {0}.\n Title:{1}\nID:{2}\nBody:{3}", userId, title, id, body);
+        }
+    }
+}
---
+++ Web-Services-and-Cloud-master/04. Consuming-Web-Services-using-CSharp/homework/Homework/Homework/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace Homework
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Linq;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        { 
+            Console.WriteLine("Please input title, which you search(for all empty string).");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Please input count of post?");
+
+            int count = 0;
+
+            while(!int.TryParse(Console.ReadLine(),out count))
+            {
+                Console.WriteLine("Please put valid count.");
+            }
+
+            WebClient client = new WebClient();
+            var jsonContent = client.DownloadString("http://jsonplaceholder.typicode.com/posts");
+
+            var posts = JsonConvert.DeserializeObject<IEnumerable<Post>>(jsonContent)
+                .Where(x=> x.title.ToLower().Contains(title.ToLower()))
+                .Take(count);
+
+            foreach(var post in posts)
+            {
+                Console.WriteLine(post);
+            }
+        }
+    }
+}
