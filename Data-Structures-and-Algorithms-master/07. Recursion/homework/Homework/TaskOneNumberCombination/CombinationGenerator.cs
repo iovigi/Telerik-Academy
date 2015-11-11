58d109d5945ae07acff9681e1a6e040a3895bab9
Iliya Nedelchev <Il.Nedelchev@gmail.com>
2015-11-11 03:03:45 +0200

    homework start

---
+++ Data-Structures-and-Algorithms-master/07. Recursion/homework/Homework/TaskOneNumberCombination/CombinationGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace TaskOneNumberCombination
+{
+    using System;
+
+    public class CombinationGenerator
+    {
+        public readonly int N;
+
+        public CombinationGenerator(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("invalid n");
+            }
+
+            this.N = n;
+        }
+
+        public IEnumerable<int[]> GetCombination()
+        {
+            List<int[]> combinations = new List<int[]>();
+
+            this.GetCombination(ref combinations, new int[this.N], 0);
+
+            return combinations;
+        }
+
+
+
+        private void GetCombination(ref List<int[]> combinations, int[] buffer, int currentIndex)
+        {
+            if (currentIndex == this.N)
+            {
+                combinations.Add(buffer);
+
+                return;
+            }
+
+            buffer[currentIndex] = 1;
+            this.GetCombination(ref combinations, buffer, currentIndex + 1);
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms-master/07. Recursion/homework/Homework/TaskOneNumberCombination/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskOneNumberCombination
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CombinationGenerator generator = new CombinationGenerator(2);
+
+            foreach (var buffer in generator.GetCombination())
+            {
+                Console.WriteLine(string.Join(" ",buffer));
+            }
+        }
+    }
+}
