4df150eae4117597b6e6bec10abea5f4b1325ad8
iovigi <il.nedelchev@gmail.com>
2015-11-11 20:47:16 +0200

    three

---
+++ Data-Structures-and-Algorithms-master/05. Advanced-Data-Structures/homework/ViolentException/ViolentException/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViolentException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new AccessViolationException();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms-master/07. Recursion/homework/Homework/TaskThreeCombinationWithoutDublication/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskThreeCombinationWithoutDublication
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<int[]> combinations = new List<int[]>();
+
+            GetCombination(ref combinations, new int[] { 0, 0 }, 0, 1, 5);
+
+            Console.WriteLine(string.Join(",", combinations.Select(x => string.Format("({0})", string.Join(" ", x)))));
+        }
+
+        private static void GetCombination(ref List<int[]> combinations, int[] buffer, int currentIndex, int value, int max)
+        {
+            if (currentIndex == buffer.Length)
+            {
+                int[] temp = new int[buffer.Length];
+                buffer.CopyTo(temp, 0);
+
+                combinations.Add(temp);
+
+                return;
+            }
+
+            if (value > max)
+            {
+                return;
+            }
+
+            buffer[currentIndex] = value;
+            GetCombination(ref combinations, buffer, currentIndex + 1, value + 1, max);
+
+            if (value < max)
+            {
+                GetCombination(ref combinations, buffer, currentIndex, value + 1, max);
+            }
+        }
+    }
+}
