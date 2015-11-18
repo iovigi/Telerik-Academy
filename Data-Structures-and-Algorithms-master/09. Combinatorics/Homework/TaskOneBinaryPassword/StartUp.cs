474bcf293ccbc1a461ad2d7419581041199749b2
Iliya Nedelchev <Il.Nedelchev@gmail.com>
2015-11-18 03:55:50 +0200

    homework

---
+++ Data-Structures-and-Algorithms-master/09. Combinatorics/Homework/TaskOneBinaryPassword/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace TaskOneBinaryPassword
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var password = Console.ReadLine();
+
+            int countOfUknown = 0;
+
+            foreach (var letter in password)
+            {
+                if (letter == '*')
+                {
+                    countOfUknown++;
+                }
+            }
+
+            long result = 1;
+
+            for (int i = 0; i < countOfUknown; i++)
+            {
+                result *= 2;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms-master/09. Combinatorics/Homework/TaskTwoColorfulRabbits/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace TaskTwoColorfulRabbits
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int countOfRabbits = int.Parse(Console.ReadLine());
+
+            int[] rabbits = new int[1000002];
+            int result = 0;
+            for (int i = 0; i < countOfRabbits; i++)
+            {
+                int rabbitAnswer = int.Parse(Console.ReadLine()) + 1;
+                rabbits[rabbitAnswer]++;
+            }
+
+            for (int i = 0; i < rabbits.Length; i++)
+            {
+                if (rabbits[i] == 0)
+                {
+                    continue;
+                }
+
+                if (rabbits[i] <= i)
+                {
+                    result += i;
+                }
+                else
+                {
+                    result += (int)Math.Ceiling((double)rabbits[i] / i) * i;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
