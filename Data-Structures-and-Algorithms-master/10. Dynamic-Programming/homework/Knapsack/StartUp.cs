87710ea64ebbca1f8a0fc59ce6320c67537647e0
iovigi <il.nedelchev@gmail.com>
2015-11-25 18:45:20 +0200

    homework start

---
+++ Data-Structures-and-Algorithms-master/10. Dynamic-Programming/homework/Knapsack/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace Knapsack
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public static int MaxWeight(int[,] items, bool[] isUsed, int currentWeight, int currentPrice, int maxWeight)
+        {
+            int maxPrice = currentPrice;
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (!isUsed[i] && items[i, 0] + currentWeight <= maxWeight && maxPrice < currentPrice + items[i, 1])
+                {
+                    isUsed[i] = true;
+                    maxPrice = MaxWeight(items, isUsed, items[i, 0] + currentWeight, maxPrice + items[i, 1], maxWeight);
+                    isUsed[i] = false;
+                }
+            }
+
+            return maxPrice;
+        }
+    }
+}
