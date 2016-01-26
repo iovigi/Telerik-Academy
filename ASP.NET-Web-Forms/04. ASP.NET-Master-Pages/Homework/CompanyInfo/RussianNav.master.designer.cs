fb6e27a51d2dbc1e759cd3294b4dc3ef85f51e9b
iovigi <il.nedelchev@gmail.com>
2016-01-26 00:24:41 -0800

    homework

--- ASP.NET-Web-Forms/04. ASP.NET-Master-Pages/Homework/CompanyInfo/RussianNav.master.designer.cs
+++ ASP.NET-Web-Forms/04. ASP.NET-Master-Pages/Homework/CompanyInfo/RussianNav.master.designer.cs
@@ -13,6 +13,15 @@ namespace CompanyInfo {
     public partial class RussianNav {
         
         /// <summary>
+        /// headRU control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder headRU;
+        
+        /// <summary>
         /// ContentRU control.
         /// </summary>
         /// <remarks>
